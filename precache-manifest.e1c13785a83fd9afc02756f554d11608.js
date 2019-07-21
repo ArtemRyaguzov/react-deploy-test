58d846de21fd253ace9e31e8f09d32c06abb015d
Artem Ryaguzov <artemkaryaguzov@gmail.com>
2019-07-21 17:35:25 +0400

    Updates

---
+++ precache-manifest.e1c13785a83fd9afc02756f554d11608.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6b139870e5d318300037e3b0ab9edec",
+    "url": "/react-deploy-test/index.html"
+  },
+  {
+    "revision": "301ad849a7c0dd4ed692",
+    "url": "/react-deploy-test/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/react-deploy-test/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "301ad849a7c0dd4ed692",
+    "url": "/react-deploy-test/static/js/main.b9e2d134.chunk.js"
+  },
+  {
+    "revision": "159089ec984b0660a05a",
+    "url": "/react-deploy-test/static/js/runtime~main.57368b07.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deploy-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1a14b5c744aca6cc890462e628b708fd.js"
+  "/react-deploy-test/precache-manifest.e1c13785a83fd9afc02756f554d11608.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-deploy-test/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
